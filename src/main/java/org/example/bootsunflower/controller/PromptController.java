7868dfbcae5f6af6fcbdd519379074fdfccae8b1
dochmai382 <dochmai382@gmail.com>
2025-06-11 11:28:01 +0900

    form 생성

--- src/main/java/org/example/bootsunflower/controller/PromptController.java
+++ src/main/java/org/example/bootsunflower/controller/PromptController.java
@@ -1,8 +1,11 @@
 package org.example.bootsunflower.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.bootsunflower.dto.PromptForm;
 import org.example.bootsunflower.service.PromptService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -10,7 +13,9 @@ public class PromptController {
 
     private final PromptService promptService;
 
-    public String index() {
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("promptForm", new PromptForm(""));
         return "index";
     }
 }
